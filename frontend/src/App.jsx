1e71f975e3cba07a595eec4b1cb580252ee32493
filipivanovic <filip_ivanovic@hotmail.com>
2025-04-22 21:34:37 +0200

    Signup functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,14 @@ import ProfilePage from './pages/ProfilePage.jsx'
 import { useAuthStore } from './store/useAuthStore.js'
 import { useEffect } from 'react'
 import { Loader } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
 
   useEffect(() => {
     checkAuth()
-  }, [checkAuth])
+  }, [ checkAuth ])
   console.log({ authUser })
 
   if (isCheckingAuth && !authUser)
@@ -29,22 +30,24 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          element={ authUser ? <HomePage /> : <Navigate to="/login" /> }
         />
         <Route
           path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          element={ !authUser ? <SignUpPage /> : <Navigate to="/" /> }
         />
         <Route
           path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          element={ !authUser ? <LoginPage /> : <Navigate to="/" /> }
         />
-        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/settings" element={ <SettingsPage /> } />
         <Route
           path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          element={ authUser ? <ProfilePage /> : <Navigate to="/login" /> }
         />
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,18 +1,36 @@
 import { create } from 'zustand/react'
 import { axiosInstance } from '../lib/axios.js'
+import toast from 'react-hot-toast'
 
 export const useAuthStore = create(set => ({
   authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
   isCheckingAuth: true,
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get('/auth/check')
       set({ authUser: res.data })
-    } catch (error) {
-      console.error(`Error in checkAuth: ${error.message}`)
+    } catch ( error ) {
+      console.error(`Error in checkAuth: ${ error.message }`)
       set({ authUser: null })
     } finally {
       set({ isCheckingAuth: false })
+    }
+  },
+  signup: async (data) => {
+    console.log('test2')
+    set({ isSigningUp: true })
+    try {
+      const res = await axiosInstance.post('/auth/signup', data)
+      set({ authUser: res.data })
+      toast.success('Signup successful')
+    } catch ( error ) {
+      console.error(`Error in signup: ${ error.message }`)
+      toast.error('Signup failed: ' + error.response.data.message)
+    } finally {
+      set({ isSigningUp: false })
     }
   }
 }))
