1a9c46b1db3c60fa361e8b87d82f118259e2547c
filipivanovic <filip_ivanovic@hotmail.com>
2025-04-23 11:14:02 +0200

    Homepage, NoChatSelected and chat store created

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,36 @@
+import { create } from 'zustand/react'
+import { axiosInstance } from '../lib/axios.js'
+import toast from 'react-hot-toast'
+
+export const useChatStore = create(set => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true })
+    try {
+      const res = await axiosInstance('/messages/users')
+      set({ users: res.data })
+    } catch (error) {
+      toast.error('Something went wrong: ' + error.response.data.message)
+      console.error(`Something went wrong: ${error.message}`)
+    } finally {
+      set({ isUsersLoading: false })
+    }
+  },
+  getMessages: async userId => {
+    set({ isMessagesLoading: true })
+    try {
+      const res = await axiosInstance(`/messages/${userId}`)
+      set({ messages: res.data })
+    } catch (error) {
+      toast.error('Something went wrong: ' + error.response.data.message)
+      console.error(`Something went wrong: ${error.message}`)
+    } finally {
+      set({ isMessagesLoading: false })
+    }
+  }
+}))
