1fde878ff04881028e44df4f948bc7ccd352489b
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-04-23 17:52:39 +0200

    MessageSkeleton.jsx implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,6 +2,7 @@ import { useChatStore } from '../store/useChatStore.js'
 import { useEffect } from 'react'
 import ChatHeader from './ChatHeader.jsx'
 import MessageInput from './MessageInput.jsx'
+import MessageSkeleton from './skeletons/MessageSkeleton.jsx'
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore()
@@ -10,7 +11,14 @@ const ChatContainer = () => {
     getMessages(selectedUser._id)
   }, [getMessages, selectedUser._id])
 
-  if (isMessagesLoading) return <div>Loading...</div>
+  if (isMessagesLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    )
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
