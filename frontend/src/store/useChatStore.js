fba7c63e42006bfad80f6b137c702fb0552c4230
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-04-23 18:50:39 +0200

    chat functionality added - without websocket implementation

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand/react'
 import { axiosInstance } from '../lib/axios.js'
 import toast from 'react-hot-toast'
 
-export const useChatStore = create(set => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -31,6 +31,16 @@ export const useChatStore = create(set => ({
       console.error(`Something went wrong: ${error.message}`)
     } finally {
       set({ isMessagesLoading: false })
+    }
+  },
+  sendMessage: async messageData => {
+    const { selectedUser, messages } = get()
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData)
+      set({ messages: [...messages, res.data] })
+    } catch (error) {
+      toast.error('Something went wrong: ' + error.response.data.message)
+      console.error(`Something went wrong: ${error.message}`)
     }
   },
   setSelectedUser: selectedUser => {
