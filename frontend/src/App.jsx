3004dd0eaf3bbd3ebf9b6198368c0c9ef40e819d
filipivanovic <filip_ivanovic@hotmail.com>
2025-04-23 10:05:10 +0200

    Settings page and themes funtionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,18 @@ import LoginPage from './pages/LoginPage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 import { useAuthStore } from './store/useAuthStore.js'
+import { useThemeStore } from './store/useThemeStore.js'
 import { useEffect } from 'react'
 import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+  const { theme } = useThemeStore()
 
   useEffect(() => {
     checkAuth()
-  }, [ checkAuth ])
+  }, [checkAuth])
   console.log({ authUser })
 
   if (isCheckingAuth && !authUser)
@@ -25,25 +27,25 @@ const App = () => {
       </div>
     )
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
           path="/"
-          element={ authUser ? <HomePage /> : <Navigate to="/login" /> }
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
         />
         <Route
           path="/signup"
-          element={ !authUser ? <SignUpPage /> : <Navigate to="/" /> }
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
         />
         <Route
           path="/login"
-          element={ !authUser ? <LoginPage /> : <Navigate to="/" /> }
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
         />
-        <Route path="/settings" element={ <SettingsPage /> } />
+        <Route path="/settings" element={<SettingsPage />} />
         <Route
           path="/profile"
-          element={ authUser ? <ProfilePage /> : <Navigate to="/login" /> }
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
         />
       </Routes>
 
