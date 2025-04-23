acd224498d9cb5dcb4e190b2d02f89bfc01e93ee
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-04-23 17:24:26 +0200

    ChatContainer.jsx, ChatHeader.jsx and MessageInput.jsx created

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,5 +1,16 @@
+import { useChatStore } from '../store/useChatStore.js'
+import { useEffect } from 'react'
+
 const ChatContainer = () => {
-  return <div>ChatContainer</div>
+  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore()
+
+  useEffect(() => {
+    getMessages(selectedUser._id)
+  }, [getMessages, selectedUser._id])
+
+  if (isMessagesLoading) return <div>Loading...</div>
+
+  return <div className="flex-1 flex flex-col overflow-auto"></div>
 }
 
 export default ChatContainer
