4760c93d0cb88ff51eedb5934c1f6d48f90574fb
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-04-24 15:57:00 +0200

    additional deployment fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
+import path from 'path'
+
 import authRoutes from '../routes/auth.route.js'
 import messageRoutes from '../routes/message.route.js'
 import { connectDB } from '../lib/db.js'
@@ -13,6 +15,7 @@ dotenv.config()
 // const app = express()
 
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -25,6 +28,14 @@ app.use(
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
